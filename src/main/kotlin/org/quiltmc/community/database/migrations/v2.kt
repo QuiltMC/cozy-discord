970018f1b39fb57154a18854437a1e83f16ac35f
Gareth Coles <gdude2002@gmail.com>
2021-08-02 17:15:27 +0100

    Update v2 migration for consistency

--- src/main/kotlin/org/quiltmc/community/database/migrations/v2.kt
+++ src/main/kotlin/org/quiltmc/community/database/migrations/v2.kt
@@ -1,8 +1,24 @@
 package org.quiltmc.community.database.migrations
 
 import org.litote.kmongo.coroutine.CoroutineDatabase
+import org.litote.kmongo.exists
+import org.litote.kmongo.setValue
 import org.quiltmc.community.database.collections.CollabServerSettingsCollection
+import org.quiltmc.community.database.collections.SuggestionsCollection
+import org.quiltmc.community.database.entities.Suggestion
 
 suspend fun v2(db: CoroutineDatabase) {
     db.createCollection(CollabServerSettingsCollection.name)
+
+    with(db.getCollection<Suggestion>(SuggestionsCollection.name)) {
+        updateMany(
+            Suggestion::thread exists false,
+            setValue(Suggestion::thread, null),
+        )
+
+        updateMany(
+            Suggestion::threadButtons exists false,
+            setValue(Suggestion::threadButtons, null),
+        )
+    }
 }
