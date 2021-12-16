91e5c987b19cff35fda2dae07f4801e7649d02b2
Gareth Coles <gdude2002@gmail.com>
2021-12-16 20:30:46 +0000

    Add some logging to the bundled PK API

--- src/main/kotlin/org/quiltmc/community/api/pluralkit/PluralKit.kt
+++ src/main/kotlin/org/quiltmc/community/api/pluralkit/PluralKit.kt
@@ -8,11 +8,14 @@ import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
 import kotlinx.serialization.json.Json
+import mu.KotlinLogging
 
 internal const val PK_BASE_URL = "https://api.pluralkit.me/v2"
 internal const val MESSAGE_URL = "$PK_BASE_URL/messages/{id}"
 
 class PluralKit {
+    private val logger = KotlinLogging.logger { }
+
     private val client = HttpClient {
         install(JsonFeature) {
             this.serializer = KotlinxSerializer(
@@ -29,7 +32,17 @@ class PluralKit {
     suspend fun getMessage(id: String): PKMessage {
         val url = MESSAGE_URL.replace("id" to id)
 
-        return client.get(url)
+        try {
+            val result: PKMessage = client.get(url)
+
+            logger.info { "/messages/$id -> 200" }
+
+            return result
+        } catch (e: ClientRequestException) {
+            logger.info { "/messages/$id -> ${e.response.status}" }
+
+            throw e
+        }
     }
 
     suspend fun getMessageOrNull(id: Snowflake) =
@@ -47,7 +60,7 @@ class PluralKit {
         return null
     }
 
-    fun String.replace(vararg pairs: Pair<String, Any>): String {
+    private fun String.replace(vararg pairs: Pair<String, Any>): String {
         var result = this
 
         pairs.forEach { (k, v) ->
