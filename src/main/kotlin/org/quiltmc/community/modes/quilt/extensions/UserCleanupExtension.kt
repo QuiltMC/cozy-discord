4b6af60b04ac9b1748e0b94fb4b2be6dcb0a2fa1
Gareth Coles <gdude2002@gmail.com>
2021-12-20 18:56:01 +0000

    Cleanup: Add dry-run mode, remove task for now

--- src/main/kotlin/org/quiltmc/community/modes/quilt/extensions/UserCleanupExtension.kt
+++ src/main/kotlin/org/quiltmc/community/modes/quilt/extensions/UserCleanupExtension.kt
@@ -2,12 +2,19 @@
 
 package org.quiltmc.community.modes.quilt.extensions
 
+import com.kotlindiscord.kord.extensions.DISCORD_BLURPLE
+import com.kotlindiscord.kord.extensions.DISCORD_FUCHSIA
+import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.converters.impl.defaultingBoolean
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.ephemeralSlashCommand
+import com.kotlindiscord.kord.extensions.time.TimestampType
+import com.kotlindiscord.kord.extensions.time.toDiscord
+import com.kotlindiscord.kord.extensions.types.editingPaginator
 import com.kotlindiscord.kord.extensions.types.respond
-import com.kotlindiscord.kord.extensions.utils.scheduling.Scheduler
 import com.kotlindiscord.kord.extensions.utils.scheduling.Task
 import dev.kord.common.entity.Permission
+import dev.kord.core.entity.Member
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.toList
 import kotlinx.datetime.Clock
@@ -19,8 +26,9 @@ import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
 private const val MAX_PENDING_DAYS = 3
+private const val MEMBER_CHUNK_SIZE = 10
 
-private val TASK_DELAY = Duration.hours(1)
+// private val TASK_DELAY = Duration.hours(1)
 private val MAX_PENDING_DURATION = Duration.days(MAX_PENDING_DAYS)
 
 class UserCleanupExtension : Extension() {
@@ -28,14 +36,14 @@ class UserCleanupExtension : Extension() {
 
     private val servers: ServerSettingsCollection by inject()
 
-    private val scheduler = Scheduler()
+    // private val scheduler = Scheduler()
     private lateinit var task: Task
 
     override suspend fun setup() {
-        task = scheduler.schedule(TASK_DELAY, pollingSeconds = 60, callback = ::taskRun)
+//        task = scheduler.schedule(TASK_DELAY, pollingSeconds = 60, callback = ::taskRun)
 
         GUILDS.forEach { guildId ->
-            ephemeralSlashCommand {
+            ephemeralSlashCommand(::CleanupArgs) {
                 name = "cleanup-users"
                 description = "Clean up likely bot accounts"
 
@@ -44,18 +52,64 @@ class UserCleanupExtension : Extension() {
                 check { hasPermissionInMainGuild(Permission.Administrator) }
 
                 action {
-                    val removed = taskRun()
-
-                    respond {
-                        content = "Kicked $removed accounts across all Quilt servers."
+                    if (::task.isInitialized) {
+                        task.cancel()
                     }
+
+                    val guilds = servers.getByQuiltServers().toList().sortedBy { it.quiltServerType!!.readableName }
+                    val removed = taskRun(arguments.dryRun).groupBy { it.guildId }
+
+                    if (removed.isEmpty()) {
+                        respond {
+                            content = "It doesn't look like there's anyone that needs to be removed."
+                        }
+
+                        return@action
+                    }
+
+                    editingPaginator {
+                        var defaultSet = false
+
+                        for (guild in guilds) {
+                            val members = removed[guild._id] ?: continue
+
+                            if (members.isEmpty()) {
+                                continue
+                            }
+
+                            if (!defaultSet) {
+                                defaultSet = true
+
+                                pages.defaultGroup = guild.quiltServerType!!.readableName
+                            }
+
+                            members.chunked(MEMBER_CHUNK_SIZE).forEach { chunk ->
+                                page(guild.quiltServerType!!.readableName) {
+                                    color = DISCORD_FUCHSIA
+                                    title = "User Cleanup: ${guild.quiltServerType!!.readableName}"
+
+                                    if (arguments.dryRun) {
+                                        color = DISCORD_BLURPLE
+                                        title += " (dry-run)"
+                                    }
+
+                                    description = "**Member** | **Join date**\n\n"
+
+                                    chunk.forEach { member ->
+                                        description += "${member.mention} | " +
+                                                "${member.joinedAt.toDiscord(TimestampType.Default)}\n"
+                                    }
+                                }
+                            }
+                        }
+                    }.send()
                 }
             }
         }
     }
 
-    suspend fun taskRun(): Int {
-        var removed = 0
+    suspend fun taskRun(dryRun: Boolean = false): MutableList<Member> {
+        val removed: MutableList<Member> = mutableListOf()
         val cutoff = Clock.System.now() - MAX_PENDING_DURATION
 
         try {
@@ -69,8 +123,11 @@ class UserCleanupExtension : Extension() {
                     .filter { it.isPending && it.joinedAt <= cutoff }
                     .toList()
                     .forEach {
-                        it.kick("Failed to pass member screening within $MAX_PENDING_DAYS days.")
-                        removed += 1
+                        if (!dryRun) {
+                            it.kick("Failed to pass member screening within $MAX_PENDING_DAYS days.")
+                        }
+
+                        removed += it
                     }
             }
 
@@ -88,5 +145,13 @@ class UserCleanupExtension : Extension() {
         if (::task.isInitialized) {
             task.cancel()
         }
+    }
+
+    inner class CleanupArgs : Arguments() {
+        val dryRun by defaultingBoolean(
+            "dry-run",
+            "Whether to preview the member to kick instead of actually kicking them",
+            true
+        )
     }
 }
