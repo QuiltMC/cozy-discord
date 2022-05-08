22cea717e6fde36fe9fddcbbfab6ad95b8d42094
Matteo Bertucci <matteobertucci2004@gmail.com>
2022-05-08 16:49:47 +0200

    Add role syncing module
    This module will sync source and target roles. When a user gets added or removed the source role, the bot will copy those changes to the target role.
    
    Roles can span across guilds. The `/sync-role` command can also be used to manually sync the roles.
    
    The module is already configured to sync the "Quilt Developer" role from the Toolchain server to the Community server.

---
+++ module-role-sync/src/main/kotlin/org/quiltmc/community/cozy/modules/rolesync/config/RoleSyncConfig.kt
@@ -0,0 +1,39 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package org.quiltmc.community.cozy.modules.rolesync.config
+
+import com.kotlindiscord.kord.extensions.checks.types.Check
+import dev.kord.common.entity.Snowflake
+
+/**
+ * The roles to sync between the two guilds.
+ *
+ * @param source The source role to sync. Modifications to this role will be synced to the target role.
+ * @param target The target role that will be synced when the source role is changed.
+ */
+public data class RoleToSync(
+    val source: Snowflake,
+    val target: Snowflake,
+)
+
+/**
+ * Class representing the configuration for the role sync module.
+ *
+ * Extend this class to implement your configuration loader. If you only need a basic config, then you can
+ * take a look at [SimpleRoleSyncConfig] instead.
+ */
+public open class RoleSyncConfig {
+    /**
+     * Override this to specify the roles that should be synced.
+     */
+    public open suspend fun getRolesToSync(): List<RoleToSync> = listOf()
+
+    /**
+     * Override this to provide a list of KordEx [Check]s that must pass for commands to be executed.
+     */
+    public open suspend fun getCommandChecks(): List<Check<*>> = listOf()
+}
---
+++ module-role-sync/src/main/kotlin/org/quiltmc/community/cozy/modules/rolesync/config/SimpleRoleSyncConfig.kt
@@ -0,0 +1,42 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package org.quiltmc.community.cozy.modules.rolesync.config
+
+import com.kotlindiscord.kord.extensions.checks.types.Check
+import dev.kord.common.entity.Snowflake
+
+public class SimpleRoleSyncConfig(builder: Builder) : RoleSyncConfig() {
+    private val rolesToSync: MutableList<RoleToSync> = builder.rolesToSync
+    private val commandChecks: MutableList<Check<*>> = builder.commandChecks
+
+    override suspend fun getRolesToSync(): List<RoleToSync> =
+        rolesToSync
+
+    override suspend fun getCommandChecks(): List<Check<*>> =
+        commandChecks
+
+    public class Builder {
+        internal var rolesToSync: MutableList<RoleToSync> = mutableListOf()
+        internal val commandChecks: MutableList<Check<*>> = mutableListOf()
+
+        public fun roleToSync(source: Snowflake, target: Snowflake) {
+            rolesToSync.add(RoleToSync(source, target))
+        }
+
+        public fun commandCheck(body: Check<*>) {
+            commandChecks.add(body)
+        }
+    }
+}
+
+public inline fun SimpleRoleSyncConfig(body: SimpleRoleSyncConfig.Builder.() -> Unit): SimpleRoleSyncConfig {
+    val builder = SimpleRoleSyncConfig.Builder()
+
+    body(builder)
+
+    return SimpleRoleSyncConfig(builder)
+}
