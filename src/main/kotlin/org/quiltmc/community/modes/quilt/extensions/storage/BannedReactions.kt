3468cda2bd15cddb53a62e3a2c4b22d2bebc2811
Gareth Coles <gdude2002@gmail.com>
2022-07-11 16:00:04 +0100

    🚧 Switch to storage system, fix up text

---
+++ src/main/kotlin/org/quiltmc/community/modes/quilt/extensions/storage/BannedReactions.kt
@@ -0,0 +1,17 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package org.quiltmc.community.modes.quilt.extensions.storage
+
+import com.kotlindiscord.kord.extensions.storage.Data
+import dev.kord.common.entity.Snowflake
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BannedReactions(
+    val categories: MutableSet<Snowflake> = mutableSetOf(),
+    val reactionNames: MutableSet<String> = mutableSetOf(),
+) : Data
