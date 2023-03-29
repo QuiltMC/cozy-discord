QuiltMC/cozy-discord

commit ea3b420693c29244ed86e477b63313da5b938c9c

Add timeout filter action (#65)

src/main/kotlin/org/quiltmc/community/modes/quilt/extensions/filtering/FilterAction.kt
@@ -17,6 +17,7 @@ enum class FilterAction(
 ) : ChoiceEnum {
 	RESPOND(-1, "Respond with the filter note"),
 	DELETE(0, "Delete message"),
-	KICK(1, "Kick user", true),
-	BAN(2, "Ban user", true)
+	TIMEOUT(1, "Timeout user", true),
+	KICK(2, "Kick user"),
+	BAN(3, "Ban user", true)
 }

src/main/kotlin/org/quiltmc/community/modes/quilt/extensions/filtering/FilterExtension.kt
@@ -32,6 +32,7 @@ import com.kotlindiscord.kord.extensions.utils.deleteIgnoringNotFound
 import com.kotlindiscord.kord.extensions.utils.dm
 import com.kotlindiscord.kord.extensions.utils.getJumpUrl
 import com.kotlindiscord.kord.extensions.utils.respond
+import com.kotlindiscord.kord.extensions.utils.timeout
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.behavior.ban
 import dev.kord.core.behavior.channel.createEmbed
@@ -55,6 +56,7 @@ import org.quiltmc.community.database.collections.GlobalSettingsCollection
 import org.quiltmc.community.database.collections.ServerSettingsCollection
 import org.quiltmc.community.database.entities.FilterEntry
 import java.util.*
+import kotlin.time.Duration.Companion.minutes
 
 const val FILTERS_PER_PAGE = 2
 
@@ -860,6 +862,30 @@ class FilterExtension : Extension() {
 				}
 
 				message.deleteIgnoringNotFound()
+			}
+
+			FilterAction.TIMEOUT -> {
+				message.deleteIgnoringNotFound()
+
+				message.author!!.dm {
+					content = "You have been timed out on **${guild.name}** for sending the below message. It has also been removed."
+
+					embed {
+						description = message.content
+
+						field {
+							name = "Channel"
+							value = "${message.channel.mention} (`${message.channel.id.value}`)"
+						}
+
+						field {
+							name = "Message ID"
+							value = "`${message.id}`"
+						}
+					}
+				}
+
+				message.author!!.asMember(message.getGuild().id).timeout(10.minutes, "Triggered filter: $_id")
 			}
 
 			FilterAction.KICK -> {

commit 74b787d5a8946cd69aa13ce47b984aa4717a3ac5

Fix action applicability
Accidentally broken in https://github.com/QuiltMC/cozy-discord/pull/65

src/main/kotlin/org/quiltmc/community/modes/quilt/extensions/filtering/FilterAction.kt
@@ -17,7 +17,7 @@ enum class FilterAction(
 ) : ChoiceEnum {
 	RESPOND(-1, "Respond with the filter note"),
 	DELETE(0, "Delete message"),
-	TIMEOUT(1, "Timeout user", true),
-	KICK(2, "Kick user"),
+	TIMEOUT(1, "Timeout user"),
+	KICK(2, "Kick user", true),
 	BAN(3, "Ban user", true)
 }