84cf9816561da3f7ebe7d32490c96207e8aa52ee
Gareth Coles <gdude2002@gmail.com>
2023-03-29 10:21:05 +0100

    Piracy: Attempt detection of alternate auth

--- module-log-parser/src/main/kotlin/org/quiltmc/community/cozy/modules/logs/processors/PiracyProcessor.kt
+++ module-log-parser/src/main/kotlin/org/quiltmc/community/cozy/modules/logs/processors/PiracyProcessor.kt
@@ -16,6 +16,14 @@ private val CHECK_REGEX = "Failed to verify authentication\n.+?401\n".toRegex()
 private const val QUILT_DEV_TEXT = "Loaded Quilt development mappings for mixin remapper!"
 private const val FABRIC_DEV_TEXT = "at net.fabricmc.devlaunchinjector.Main.main(Main.java"
 
+private val ALTERNATIVE_AUTH_FRAGMENTS = arrayOf(
+	"[authlib-injector]",
+	"authlib-injector.log",
+	"authlib-injector.jar",
+	"Yggdrasil Public Key Transformer",
+	"Authentication server: https?://\\S",
+).map { it.toRegex(RegexOption.IGNORE_CASE) }
+
 public class PiracyProcessor : LogProcessor() {
 	override val identifier: String = "piracy"
 	override val order: Order = Order.Earlier
@@ -31,7 +39,7 @@ public class PiracyProcessor : LogProcessor() {
 				"**You seem to be using TLauncher.**\n\n" +
 
 						"TLauncher is widely-known throughout the Minecraft community as a piracy tool. As all " +
-						"communities on Discord must obey its the Terms of Service, this bot is unable to " +
+						"communities on Discord must obey the Terms of Service, this bot is unable to " +
 						"provide you with support - as long as you make use of this launcher. This is the " +
 						"case regardless of what you've heard elsewhere, or what servers have yet to have " +
 						"been banned from Discord.\n\n" +
@@ -41,6 +49,20 @@ public class PiracyProcessor : LogProcessor() {
 			)
 
 			return
+		}
+
+		if (ALTERNATIVE_AUTH_FRAGMENTS.any { it in log.content }) {
+			log.abort(
+				"**You seem to be using an alternative Minecraft authentication provider.**\n\n" +
+
+					"The only legal way to play Minecraft is by buying a copy of it from Mojang, and using your " +
+					"paid account to play. As all communities on Discord must obey the Terms of Service, this " +
+					"bot is unable to provide you with support. This is the case regardless of what you've heard " +
+					"elsewhere, or what servers have yet to have been banned from Discord.\n\n" +
+
+					"Please bear in mind that neither this server's staff, nor the developers working on " +
+					"this bot, are unable to do anything about the Discord Terms of Service."
+			)
 		}
 
 		CHECK_REGEX.find(log.content)
