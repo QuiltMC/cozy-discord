e0fc9e8afd65d2a7323baeaef18094953bd44dc0
Gareth Coles <gdude2002@gmail.com>
2024-08-20 18:11:52 +0100

    Update Kotlin and KordEx plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,14 +17,14 @@ dependencies {
 	implementation(gradleApi())
 	implementation(localGroovy())
 
-	implementation(kotlin("gradle-plugin", version = "2.0.20-Beta1"))
-	implementation(kotlin("serialization", version = "2.0.20-Beta1"))
+	implementation(kotlin("gradle-plugin", version = "2.0.20-RC2"))
+	implementation(kotlin("serialization", version = "2.0.20-RC2"))
 
 	implementation("com.github.jakemarsden", "git-hooks-gradle-plugin", "0.0.2")
 	implementation("com.expediagroup.graphql", "com.expediagroup.graphql.gradle.plugin", "7.1.4")
 	implementation("com.github.johnrengelman.shadow", "com.github.johnrengelman.shadow.gradle.plugin", "8.1.1")
-	implementation("com.google.devtools.ksp", "com.google.devtools.ksp.gradle.plugin", "2.0.20-Beta1-1.0.22")
-	implementation("dev.kordex.gradle.plugins", "kordex", "1.3.1")
+	implementation("com.google.devtools.ksp", "com.google.devtools.ksp.gradle.plugin", "2.0.20-RC2-1.0.24")
+	implementation("dev.kordex.gradle.plugins", "kordex", "1.4.1")
 	implementation("dev.yumi", "yumi-gradle-licenser", "1.2.0")
 	implementation("io.gitlab.arturbosch.detekt", "detekt-gradle-plugin", "1.23.6")
 }
